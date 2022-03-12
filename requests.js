90e349de4694de1be295e1e6792882311591b5d2
Maximilian Kerz <kerz.maximilian@gmail.com>
2022-03-12 14:21:49 -0700

    Error handling and guard function added

--- requests.js
+++ requests.js
@@ -1,7 +1,8 @@
 const _ = require("lodash");
 const jwt_decode = require('jwt-decode');
 const config = require('../config.json');
-const ENVIRONMENT = process.env.ENVIRONMENT
+const httpCodes = require('./httpCodes.json');
+const ENVIRONMENT = process.env.ENVIRONMENT;
 
 
 /**
@@ -11,7 +12,7 @@ const ENVIRONMENT = process.env.ENVIRONMENT
  * @returns 
  */
  exports.extractParameters = (req, allowedParams) => {
-    const params = this.getParameters(req)
+    const params = getParameters(req)
     const paramsKeys = params ? Object.keys(params) : []
 
     let extractedParams = {}
@@ -25,64 +26,6 @@ const ENVIRONMENT = process.env.ENVIRONMENT
     if(allowedParams.includes("offset") && extractedParams["offset"] === undefined) extractedParams["offset"] = 0
     return extractedParams
 } 
-
-/**
- * Retrieves parameters from request (query AND route parameters)
- * @param {object} req 
- * @returns 
- */
-exports.getParameters = (req) => {
-    const params = {
-        ...req.apiGateway.event.pathParameters,
-        ...req.apiGateway.event.queryStringParameters
-    }
-    return params
-}
-
-/**
- * Parses only allowed parameter keys
- * @param {*} parameter 
- * @param {*} allowedArr 
- * @returns 
- */
-const parseParameter = (parameter, allowedArr) => {
-    if(_.includes(allowedArr, parameter)) return parameter
-    return undefined
-}
-
-/**
- * Evaluates the value(s) of suuplied allowed params 
- * @param {string} value 
- * @param {string} key 
- * @returns 
- */
-const evaluateParamValue = (value, key) => {
-    const paramInstruction = config.params[key]
-    if(paramInstruction === undefined) return undefined
-    if(!paramInstruction.requiresEval) return value
-    
-    if(paramInstruction.type === "number") {
-        try {
-            const valueNumber = parseInt(value)
-            return valueNumber
-        } catch(e) {
-            return undefined
-        }
-    }
-    if(paramInstruction.type === "string") {
-        const values = value.split("|")
-        const maxIndex = paramInstruction.allowMultipleValues ? values.length : 1
-
-        let allowedParamValues = []
-        for(let i = 0; i < maxIndex; i++) {
-            if(paramInstruction.allowedValues.includes(values[i])) allowedParamValues.push(values[i])
-        }
-
-        if(allowedParamValues.length === 0) return undefined
-        if(!paramInstruction.allowMultipleValues) return allowedParamValues[0]
-        return allowedParamValues
-    }
-}
 
 /**
  * Extracts user id from auth JWT token if present
@@ -141,4 +84,106 @@ const evaluateParamValue = (value, key) => {
     
     const requestAuthentic = userAccount["__typename"] !== undefined ? true : false
     return requestAuthentic
+}
+
+
+/**
+ * Returns a response object with correct error formating
+ * @param {string} errorName 500_INTERNAL_SERVER_ERROR by default; Choose from errors.json for full option list
+ * @param {string} description optional
+ * @returns {object} resObj for express
+ */
+ exports.createResponseObject = (body, statusName="500_INTERNAL_SERVER_ERROR", description="") => {
+    let statusType = httpCodes[statusName]
+    if(statusType === undefined) statusType = httpCodes["500_INTERNAL_SERVER_ERROR"]
+    let resObj = { 
+        statusCode: statusType.code, 
+        status: statusType.message, 
+        description, 
+        body:{}
+    }
+    if(resObj.statusCode >= 200 && resObj.statusCode < 300) resObj["body"] = { ...body }
+    if(resObj.statusCode >= 300 ) resObj["body"] = { status: statusType.message, description }
+    return resObj
+}
+
+/**
+ * A guardian wrapper which catches any issues going wrong and returns a 500 error
+ * @param {function} func 
+ * @param  {...any} args 
+ * @returns 
+ */
+exports.guard = async (func, ...args) => {
+    try {
+        const resObj = await func(...args)
+        return resObj
+    } catch(e) {
+        console.error(e)
+        const resObj = this.createResponseObject(
+            {}, 
+            "500_INTERNAL_SERVER_ERROR", 
+            "Guard detected a server-side malfunction."
+        )
+        return resObj
+    }
+}
+
+//// Private support functions ////////////////////////////////////////////////////////////////////
+
+/**
+ * Retrieves parameters from request (query AND route parameters)
+ * @param {object} req 
+ * @returns 
+ */
+ const getParameters = (req) => {
+    const params = {
+        ...req.apiGateway.event.pathParameters,
+        ...req.apiGateway.event.queryStringParameters
+    }
+    return params
+}
+
+/**
+ * Parses only allowed parameter keys
+ * @param {*} parameter 
+ * @param {*} allowedArr 
+ * @returns 
+ */
+const parseParameter = (parameter, allowedArr) => {
+    if(_.includes(allowedArr, parameter)) return parameter
+    return undefined
+}
+
+/**
+ * Evaluates the value(s) of suuplied allowed params 
+ * @param {string} value 
+ * @param {string} key 
+ * @returns 
+ */
+const evaluateParamValue = (value, key) => {
+    const paramInstruction = config.params[key]
+    if(paramInstruction === undefined) return undefined
+    if(!paramInstruction.requiresEval) return value
+    
+    if(paramInstruction.type === "number") {
+        try {
+            const valueNumber = parseInt(value)
+            return valueNumber
+        } catch(e) {
+            return undefined
+        }
+    }
+    if(paramInstruction.type === "string") {
+        const values = value.split("|")
+        const maxIndex = paramInstruction.allowMultipleValues ? values.length : 1
+
+        let allowedParamValues = []
+        for(let i = 0; i < maxIndex; i++) {
+            if(paramInstruction.allowedValues.includes(values[i])) allowedParamValues.push(values[i])
+        }
+
+        if(allowedParamValues.length === 0) return undefined
+        if(!paramInstruction.allowMultipleValues) return allowedParamValues[0]
+        return allowedParamValues
+    }
 }
